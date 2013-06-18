7d179cd631ea561622f437180ba007d680eb664f
vassilis <vasilhsfoto@gmail.com>
2013-06-18 23:21:58 +0300

    Deleting 2 interfaces

--- src/main/java/com/vassilis/services/FileCacheLoader.java
+++
@@ -1,8 +0,0 @@
-package com.vassilis.services;
-
-import java.io.File;
-import java.util.Set;
-
-public interface FileCacheLoader {
-	Set<String> loadFileInCache(File file);
-}
--- src/main/java/com/vassilis/services/FindCommonWords.java
+++
@@ -1,8 +0,0 @@
-package com.vassilis.services;
-
-import java.io.File;
-import java.util.Set;
-
-public interface FindCommonWords {
-	void findCommonWords(Set<String> smallFileCache, File file, File destinationFile);
-}
